b35991a9d4c50b22073532fa02f173147905165b
LeeVicent <1003766125@qq.com>
2018-01-18 20:43:08 +0800

    1.将EditText改为AutoCompleteEditText

---
+++ app/src/main/java/com/vicent/pushnotification/ui/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.vicent.pushnotification.ui.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+import com.vicent.pushnotification.R;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "销毁", Toast.LENGTH_SHORT).show();
+    }
+}
