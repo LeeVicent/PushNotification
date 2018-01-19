6e862d996ca99fc235cab10d208ad40e9aafcdef
LeeVicent <1003766125@qq.com>
2018-01-19 17:44:27 +0800

    推送至GitHub

--- app/src/main/java/com/vicent/pushnotification/ui/activity/MainActivity.java
+++ app/src/main/java/com/vicent/pushnotification/ui/activity/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+
         setOnClickListeners();
     }
 
